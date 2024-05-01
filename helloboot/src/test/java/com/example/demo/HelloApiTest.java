501f05a534c64fe034dd75bb2bd68973dc3964df
sweet.dh <dlsrksrhk@gmail.com>
2024-05-01 11:36:13 +0900

    플젝초기화

---
+++ helloboot/src/test/java/com/example/demo/HelloApiTest.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HelloApiTest {
+
+    @Test
+    public void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        //200 ok res
+        //hearder content-type이 text/plain으로 시작
+        //body Hello Spring
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE))
+                .startsWith(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+
+}
